272dcef947affbeaa6842a99a9f8245f0ffbce30
1211059-Gonçalo Noronha <1211059@isep.ipp.pt>
2023-12-13 14:13:53 +0000

    Small changes

---
+++ App.js
@@ -0,0 +1,19 @@
+import React, {Component} from ’react’;
+import {View, Text} from ’react-native’;
+class App extends Component {
+constructor(props) {
+super(props);
+this.state = {
+title: ’DSSMV: React-native example’,
+};
+}
+render() {
+const title = this.state.title;
+return (
+<View>
+<Text>{title}</Text>
+</View>
+);
+}
+}
+export default App;
