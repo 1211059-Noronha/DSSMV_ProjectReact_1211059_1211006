3f3631eaa16a80228bc855c28c59a42f86ab516b
BeatrizLago <1211006@isep.ipp.pt>
2023-12-15 17:03:25 +0000

    buttons e imagem (com erros :/) (primeiro commit estava errado)

--- App.js
+++ App.js
@@ -1,31 +1,9 @@
 import React, {Component} from 'react';
-import {View, Text, SafeAreaView, Image, Button} from 'react-native';
-class App extends Component {
-constructor(props) {
-super(props);
-this.state = {
-title: 'DSSMV: React-native example',
-};
-}
-render() {
+import {View, Text, Image} from 'react-native';
+import WelcomeScreen from "./screens/WelcomeScreen";
 
 export default function App(){
-    return (
-        <SafeAreaView style={styles.container}>
-            <Image source={require('./assets/logo_light.png')}/>
-            <Text>TrandBer</Text>
-            <Button title="Reserve a Book" onPress={() => console.log("Button Clicked")}></Button>
-            <Button title="Management" onPress={() => console.log("Button Clicked")}></Button>
-        </SafeAreaView>
-    );
+    return(
+        <WelcomeScreen />
+    )
 }
-
-const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor:"#fff",
-        justifyContent: "top"
-    },
-});
-
-}}
---
+++ screens/WelcomeScreen.js
@@ -0,0 +1,40 @@
+import React, { Component } from 'react';
+import {View, StyleSheet, Image, Text,} from 'react-native';
+import Header from '../components/Header';
+import Body from '../components/Body';
+import Footer from '../components/Footer';
+
+function WelcomeScreen(props){
+    return(
+        <View style={styles.logoContainer}
+            <Image source={require('../assets/logo_light.png')}/>
+            <Text>Your favorite Library, now online</Text>
+        </View>
+        <View style={{styles.reserveButton}}></View>
+        <View style={{styles.managementButton}}></View>
+    )
+}
+
+const styles = StyleSheet.create({
+    reserveButton: {
+        width: '100%',
+        height: 70,
+        top: 70
+    },
+    managementButton: {
+        width: '100%',
+        height: 70,
+        top: 90
+    },
+    logo_light: {
+        width: 100,
+        height: 100,
+        position: 'absolute',
+        top: 50
+    },
+    logoContainer: {
+        position: 'absolute',
+        top: 70,
+        alignItems: 'center',
+    }
+})
